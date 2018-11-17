f4c590df9711ea965734e3a66cc9d20338a67b67
csteach <csteach59@gmail.com>
2018-11-17 12:40:42 -0600

    update source

--- 422/2017/week14/BasicAppFirebase1/src/services/firebase.js
+++ 422/2017/week14/BasicAppFirebase1/src/services/firebase.js
@@ -7,12 +7,12 @@ import * as firebase from "firebase";
 
 // export firebase initialisation
 export const initialize = () => firebase.initializeApp({
-    apiKey: "AIzaSyBPcEKwXKaFfeBSzTr_A8NuJgI8GbcJrI4",
-    authDomain: "egyptian-cards.firebaseapp.com",
-    databaseURL: "https://egyptian-cards.firebaseio.com",
-    projectId: "egyptian-cards",
-    storageBucket: "egyptian-cards.appspot.com",
-    messagingSenderId: "773246184815"
+    apiKey: "_YOUR_API_KEY_",
+    authDomain: "_YOUR_APP_.firebaseapp.com",
+    databaseURL: "https://_YOUR_APP_.firebaseio.com",
+    projectId: "_YOUR_PROJECT_ID_",
+    storageBucket: "_YOUR_APP_.appspot.com",
+    messagingSenderId: "_YOUR_SENDER_ID_"
 })
 
 // setup listener for firebase updates
